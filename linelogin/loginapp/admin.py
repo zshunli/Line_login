4f5983111c49b53732e4e76fe33f4b364ec1ab2c
zshunli <shunli1313@gmail.com>
2023-01-01 03:49:58 +0800

    登入登出做出區隔，建立models

--- linelogin/loginapp/admin.py
+++ linelogin/loginapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from loginapp.models import bform
+admin.site.register(bform)
--- linelogin/loginapp/models.py
+++ linelogin/loginapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class bform(models.Model):
+    form_pk = models.CharField(max_length=100000000,primary_key=True)
+    lineuid = models.CharField(max_length=50)
+    name = models.CharField(max_length=20, blank=False)
+    nickname = models.CharField(max_length=20, blank=False)
+    phone_number = models.CharField(max_length=10, blank=False)
+    email = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=50, blank=False)
+    def _str_(self):
+        return self.form_pk
